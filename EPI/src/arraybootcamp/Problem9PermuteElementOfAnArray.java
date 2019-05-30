9a367e17f923ff3489643d6fca504055e32863f8
Gaurav Sardana <gaurav.sardana28@gmail.com>
2019-05-30 23:16:13 +0530

    Problem 9 EPI implementation added

--- EPI/src/arraybootcamp/Problem9PermuteElementOfAnArray.java
+++ EPI/src/arraybootcamp/Problem9PermuteElementOfAnArray.java
@@ -2,6 +2,7 @@ package arraybootcamp;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem9PermuteElementOfAnArray {
@@ -12,23 +13,31 @@ public class Problem9PermuteElementOfAnArray {
 		int[] arr = {1,2,3};
 		List<int[]> listInteger = permutationsInteger(arr);
 		printIntArrayList(listInteger);
+		
+		// EPI problem 9 Test
+		List<Integer> permuted = new ArrayList<Integer>(Arrays.asList(2,0,3,1));
+		List<Character> permute = new ArrayList<Character>(Arrays.asList('a','b','c','d'));
+		System.out.println(permuted);
+		permutePtoA(permuted, permute);
+		System.out.println(permute);
+		System.out.println(permuted);
 	}
 
-	
-	
-	
+
+
+
 	public static List<String> permutationsString(String str){
 		List<String> permuteStrings = new ArrayList<String>();
-		 permuteString("",str,permuteStrings);
-		 return permuteStrings;
+		permuteString("",str,permuteStrings);
+		return permuteStrings;
 	}
-	
+
 	public static void permuteString (String prefix , String suffix, List<String> results) {
-		
+
 		if(suffix.length()==0) {
 			results.add(prefix);
 		}
-		
+
 		for(int i=0;i<suffix.length();i++) {
 			permuteString(prefix+suffix.charAt(i), suffix.substring(0,i)+suffix.substring(i+1,suffix.length()), results);
 		}
@@ -39,13 +48,13 @@ public class Problem9PermuteElementOfAnArray {
 		}
 		System.out.println("Total number of permuatations are "+list.size());
 	} 
-	
+
 	public static List<int[]> permutationsInteger(int[] arr){
 		List<int[]> intPermutations = new ArrayList<int[]>();
 		permuteInt(arr,0,intPermutations);
 		return intPermutations;
 	}
-	
+
 	public static void permuteInt(int[] a, int start, List<int[]> results) {
 		if(start >=a.length) {
 			results.add(a.clone());
@@ -56,17 +65,38 @@ public class Problem9PermuteElementOfAnArray {
 			swapIntArray(a, start, i);
 		}
 	}
-	
+
 	public static void swapIntArray(int[] a, int i, int j) {
 		int temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	}
-	
+
 	public static void printIntArrayList(List<int[]> arr) {
 		for(int[] a : arr) {
 			System.out.println(Arrays.toString(a));
 		}
 		System.out.println("Total number of permuatations are "+arr.size());
+	}
+
+	//EPI problem 9 solution 
+
+	//time O(n) space O(1)
+	public static void permutePtoA(List<Integer> permuted, List<Character> permute){
+
+		for(int i=0;i<permute.size();i++) {
+			int next =i;
+			while(permuted.get(next)>=0)
+			{
+				Collections.swap(permute, i, permuted.get(next));
+				int temp = permuted.get(next);
+				permuted.set(next, permuted.get(next) - permuted.size());
+				next = temp;
+			}
+		}
+		// restore P
+		for(int i=0;i<permuted.size();i++) {
+			permuted.set(i, permuted.size()+permuted.get(i));
+		}
 	}
 }
