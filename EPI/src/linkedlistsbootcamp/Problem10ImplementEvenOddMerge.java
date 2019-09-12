5a2340c8a00cbabb44c65cc7bad05d43a2b73bc5
gaurav <gaurav.sardana28@gmail.com>
2019-09-13 00:47:56 +0530

    Link list 2 questions committed

---
+++ EPI/src/linkedlistsbootcamp/Problem10ImplementEvenOddMerge.java
@@ -0,0 +1,39 @@
+package linkedlistsbootcamp;
+
+import java.util.Arrays;
+import java.util.List;
+
+import MyLinkedList.Node;
+
+public class Problem10ImplementEvenOddMerge {
+
+	public static void main(String[] args) {
+		Node<Integer> node = new Node<Integer>(0,
+				new Node<Integer>(1, new Node<Integer>(2, new Node<Integer>(3, new Node<Integer>(4,
+						new Node<Integer>(5, new Node<Integer>(6, new Node<Integer>(7, new Node<Integer>(8)))))))));
+		 Node<Integer> evenOddMerge = evenOddMerge(node);
+		 while(evenOddMerge!=null) {
+			 System.out.println(evenOddMerge.value);
+			 evenOddMerge=evenOddMerge.nextNode;
+		 }
+		 
+
+	}
+
+	//time complexity is O(n) and space complexity O(1)
+	public static Node<Integer> evenOddMerge(Node<Integer> node) {
+		Node<Integer> dummyHeadEven = new Node<Integer>(0, null), dummyHeadOdd = new Node<Integer>(0, null);
+		List<Node<Integer>> asList = Arrays.asList(dummyHeadEven,dummyHeadOdd);
+		int temp =0;
+		for(Node<Integer> iter = node ; iter!=null;iter=iter.nextNode) {
+			asList.get(temp).nextNode = iter;
+			asList.set(temp, asList.get(0).nextNode);
+			temp^=1;
+		}
+		asList.get(1).nextNode=null;
+		asList.get(0).nextNode= dummyHeadOdd.nextNode;
+
+		return dummyHeadEven.nextNode;
+	}
+
+}
---
+++ EPI/src/linkedlistsbootcamp/Problem11TestPalindromicityOfSingleLinkedList.java
@@ -0,0 +1,58 @@
+package linkedlistsbootcamp;
+
+import MyLinkedList.Node;
+
+public class Problem11TestPalindromicityOfSingleLinkedList {
+
+	public static void main(String[] args) {
+		Node<Integer> node = 
+				new Node<Integer>(1, new Node<Integer>(2, new Node<Integer>(3, new Node<Integer>(4,
+						 new Node<Integer>(3, new Node<Integer>(2, new Node<Integer>(1)))))));
+	System.out.println(testPalindromicity(node));
+	}
+
+	//time complexity is O(n) and space complexity is O(1)
+	public static boolean testPalindromicity(Node<Integer> node) {
+		if(node== null){
+			return true;
+		}
+		Node<Integer> fast = node,slow = node;
+		while(fast.nextNode!=null && fast.nextNode.nextNode!=null) {
+			fast=fast.nextNode.nextNode;
+			slow=slow.nextNode;
+		}
+		Node<Integer> firstHalf = node;
+		Node<Integer> reverseLinkedList = reverseLinkedList(slow);
+		
+		while(firstHalf!=null && reverseLinkedList!=null) {
+			if(firstHalf.value!=reverseLinkedList.value) {
+				return false;
+			}
+			firstHalf=firstHalf.nextNode;
+			reverseLinkedList=reverseLinkedList.nextNode;
+		}
+		return true;
+	}
+
+	public static Node<Integer> reverseLinkedList(Node<Integer> node) {
+		Node<Integer> dummyHead = new Node<Integer>(0,node);
+		Node<Integer> sublistHead = dummyHead;
+		Node<Integer> temp = node;
+		int len = 0;
+		while (temp != null) {
+			len++;
+			temp = temp.nextNode;
+		}
+		Node<Integer> listIter = sublistHead.nextNode;
+		int k=1;
+		while(k++<len) {
+			Node<Integer> tempo = listIter.nextNode;
+			listIter.nextNode = tempo.nextNode;
+			tempo.nextNode = sublistHead.nextNode;
+			sublistHead.nextNode = tempo;
+		}
+		
+		return dummyHead.nextNode;
+	}
+
+}
