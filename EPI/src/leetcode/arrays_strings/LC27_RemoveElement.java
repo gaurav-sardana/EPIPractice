1ae48b6a665ea5752d396ead4173ae59b6076bf3
gaurav.sardana <gaurav.sardana@mhealth.tech>
2024-12-01 21:34:35 +0530

    added array : remove element (2 pointer, reader/writer)

--- EPI/src/leetcode/arrays_strings/LC27_RemoveElement.java
+++ EPI/src/leetcode/arrays_strings/LC27_RemoveElement.java
@@ -6,6 +6,20 @@ package leetcode.arrays_strings;
  */
 public class LC27_RemoveElement {
     public static void main(String[] args) {
+        int[] nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+        System.out.println(removeElement(nums, 2));
+    }
 
+    //two pointer approach -> reader/writer variables
+    //inplace algorithm (O(n))
+    //time : O(n), space : O(1)
+    public static int removeElement(int[] nums, int val) {
+        int writer = 0;
+        for (int reader = 0; reader < nums.length; reader++) {
+            if (nums[reader] != val) {
+                nums[writer++] = nums[reader];
+            }
+        }
+        return writer;
     }
 }
