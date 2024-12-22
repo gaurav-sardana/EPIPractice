0b13675fbc5935db7b0b1e6955ed0341be036587
gaurav.sardana <gaurav.sardana@mhealth.tech>
2024-12-22 19:00:30 +0530

    added revison

--- EPI/src/arraybootcamp/Problem6BuyAndSellAStockOnce.java
+++ EPI/src/arraybootcamp/Problem6BuyAndSellAStockOnce.java
@@ -1,18 +1,19 @@
 package arraybootcamp;
 
 public class Problem6BuyAndSellAStockOnce {
+    public static void main(String[] args) {
+        double[] arr = { 310, 315, 275, 295, 260, 270, 290, 230, 255, 350 };
+        System.out.println("Maximum Profit : " + getMaxProfit(arr));
+    }
 
-	public static void main(String[] args) {
-	double[] arr=	{310,315,275,295,260,270,290,230,255,350};
-	System.out.println("Maximum Profit : "+getMaxProfit(arr));
-	}
-
-	public static double getMaxProfit(double[] arr) {
-		double minValue = Double.MAX_VALUE,maxProfit = 0;
-		for(int i=0;i<arr.length;i++) {
-			maxProfit = Math.max(maxProfit, arr[i]-minValue);
-			minValue = Math.min(arr[i], minValue);
-		}
-		return maxProfit;
-	}
+    //selling price must be larger than buying price.
+    // we need to find max(prices[j] = prices[i]) for every i & j where j>i
+    public static double getMaxProfit(double[] arr) {
+        double minValue = Double.MAX_VALUE, maxProfit = 0;
+        for (int i = 0; i < arr.length; i++) {
+            maxProfit = Math.max(maxProfit, arr[i] - minValue);
+            minValue = Math.min(arr[i], minValue);
+        }
+        return maxProfit;
+    }
 }
--- EPI/src/leetcode/arrays_strings/LC27_RemoveElement.java
+++ EPI/src/leetcode/arrays_strings/LC27_RemoveElement.java
@@ -7,12 +7,16 @@ package leetcode.arrays_strings;
 public class LC27_RemoveElement {
     public static void main(String[] args) {
         int[] nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+        int[] nums2 = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
         System.out.println(removeElement(nums, 2));
+        System.out.println(removeElementReduceWrite(nums2, 2));
     }
 
     //two pointer approach -> reader/writer variables
     //inplace algorithm (O(n))
     //time : O(n), space : O(1)
+
+    // con : can we reudce the number of writes? as un-necessary write will happen
     public static int removeElement(int[] nums, int val) {
         int writer = 0;
         for (int reader = 0; reader < nums.length; reader++) {
@@ -21,5 +25,19 @@ public class LC27_RemoveElement {
             }
         }
         return writer;
+    }
+
+    public static int removeElementReduceWrite(int[] nums, int val) {
+        int numLength = nums.length;
+        int start = 0;
+        while (start < numLength) {
+            if (nums[start] == val) {
+                nums[start] = nums[numLength - 1];
+                numLength--;
+            } else {
+                start++;
+            }
+        }
+        return numLength;
     }
 }
